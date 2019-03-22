9b8cc4b7f579a2d129c757b2c52129973f7958cf
Robert Kurti <robert.kurti@elmatica.com>
2019-03-22 14:14:40 +0100

    Bug fix

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 "use strict";
 
 const Homey = require("homey");
+const SmartBus = require("smart-bus");
 
 class MyApp extends Homey.App {
   onInit() {
     this.log("MyApp is running...");
-    const ip = Homey.get("ip_address");
+    const ip = Homey.ManagerSettings.get("ip_address");
     this._bus = new SmartBus(`hdl://${ip}:6000`);
   }
 
